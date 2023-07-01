524b923c3a39dfced770faa8860fcfdc6a2accb6
Yvan <yvan.lokhmotov@icloud.com>
2023-07-01 22:11:27 +0100

    Added no. of images slider and favicon.ico.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,112 @@
 import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [placeholder, setPlaceholder] = useState(
-    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
-  );
-  const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_Open_AI_Key,
-  });
+	const [prompt, setPrompt] = useState("");
+	const [result, setResult] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [placeholder, setPlaceholder] = useState(
+		"Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh..",
+	);
+	const [quantity, setQuantity] = useState(3);
+	const [imageSize, setImageSize] = useState("1024x1024");
 
-  const openai = new OpenAIApi(configuration);
+	const generateImage = async () => {
+		setPlaceholder(`Search ${prompt}..`);
+		setLoading(true);
 
-  const generateImage = async () => {
-    setPlaceholder(`Search ${prompt}..`);
-    setLoading(true);
-    const res = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "512x512",
-    });
-    setLoading(false);
-    setResult(res.data.data[0].url);
-  };
-  return (
-    <div className="app-main">
-      {loading ? (
-        <>
-          <h2>Generating..Please Wait..</h2>
-          <div class="lds-ripple">
-            <div></div>
-            <div></div>
-          </div>
-        </>
-      ) : (
-        <>
-          <h2>Generate an Image using Open AI API</h2>
+		const apiUrl = import.meta.env.VITE_Open_AI_Url;
+		const openaiApiKey = import.meta.env.VITE_Open_AI_Key;
 
-          <textarea
-            className="app-input"
-            placeholder={placeholder}
-            onChange={(e) => setPrompt(e.target.value)}
-            rows="10"
-            cols="40"
-          />
-          <button onClick={generateImage}>Generate an Image</button>
-          {result.length > 0 ? (
-            <img className="result-image" src={result} alt="result" />
-          ) : (
-            <></>
-          )}
-        </>
-      )}
-    </div>
-  );
+		try {
+			const response = await fetch(apiUrl, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${openaiApiKey}`,
+				},
+				body: JSON.stringify({
+					prompt: prompt,
+					n: quantity,
+					size: imageSize,
+				}),
+			});
+
+			const data = await response.json();
+
+			setLoading(false);
+			setResult(data.data);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="app-main">
+			{loading ? (
+				<>
+					<h2>Generating your unique images... Sit tight!</h2>
+					<div className="lds-ripple">
+						<div></div>
+						<div></div>
+					</div>
+				</>
+			) : (
+				<>
+					<h2>Generate Images using AI</h2>
+					<textarea
+						className="app-input"
+						placeholder={placeholder}
+						onChange={(e) => setPrompt(e.target.value)}
+						rows="10"
+						cols="40"
+					/>
+					<label htmlFor="quantity">Number of Images:</label>
+					<input
+						id="quantity"
+						type="range"
+						min="1"
+						max="10"
+						value={quantity}
+						onChange={(e) => setQuantity(parseInt(e.target.value))}
+					/>
+					<span>{quantity}</span>
+					{/* 
+            <label htmlFor="imageSize">Image Size:</label>
+            <div className="select-container">
+              <select
+                id="imageSize"
+                value={imageSize}
+                onChange={(e) => setImageSize(e.target.value)}
+              >
+                <option value="256x256">256x256</option>
+                <option value="512x512">512x512</option>
+                <option value="1024x1024">1024x1024</option>
+              </select>
+            </div>
+          */}
+
+					<br />
+					<button onClick={generateImage}>Generate Images</button>
+					{result.length > 0 ? (
+						<div className="image-container">
+							{result.map((image, index) => (
+								<img
+									key={index}
+									className="result-image"
+									src={image.url}
+									alt={`result-${index}`}
+								/>
+							))}
+						</div>
+					) : (
+						<></>
+					)}
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
