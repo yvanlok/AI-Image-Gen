2435ec629d7e16cadd40c9d0bb8e7bf08fd75147
Yvan <yvan.lokhmotov@icloud.com>
2023-07-06 18:50:05 +0100

    Added affiliate links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { Banner } from "./728_90";
 
 function App() {
 	const [prompt, setPrompt] = useState("");
@@ -75,7 +76,7 @@ function App() {
 					<br />
 					<button onClick={generateImage}>Generate Images</button>
 					{result.length > 0 ? (
-						<div className="image-container">
+						<div className="image-container centered-div display-flex">
 							{result.map((image, index) => (
 								<img
 									key={index}
@@ -88,6 +89,22 @@ function App() {
 					) : (
 						<></>
 					)}
+					<br />
+					<a href="https://iproyal.com?r=241410" target="_blank">
+						<img
+							src="https://dashboard.iproyal.com/img/b/728_2.jpg"
+							alt="IPRoyal.com"
+						/>
+					</a>
+					<br />
+					<a href="https://dashboard.capsolver.com/passport/register?inviteCode=Ecv6Gtrh0ECa">
+						<img
+							alt="banner"
+							src="https://camo.githubusercontent.com/359a6866e8e9700c4cfa18e3bea8055e886772c93bc17618ea67ecfae8ca0d0d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313130353137323339343635353632353330362f313130353138303130313830323437313537352f32303232313230372d3136303734392e676966"
+						/>
+					</a>
+					<br />
+					<Banner />
 				</>
 			)}
 		</div>
